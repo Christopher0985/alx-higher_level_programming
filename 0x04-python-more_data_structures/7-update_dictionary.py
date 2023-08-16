619e99df5657b8f95d0ca9f866bf80cd4a98d690
Christopher0985 <ckamowatimwa@gmail.com>
2023-08-16 04:45:20 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def update_dictionary_entry(a_dict, key, value):
-    a_dict[key] = value
-    return a_dict
+def update_dictionary_entry(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
