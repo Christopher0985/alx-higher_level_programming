aed0b6d80648805d63be205d78c149dceb2b2e85
Christopher0985 <ckamowatimwa@gmail.com>
2023-08-16 03:58:23 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,4 @@
 def update_dictionary(a_dictionary, key, value):
     """Add a new key-value pair or replace an existing one in the dictionary."""
     a_dictionary[key] = value
-    return a_dictionary
+    return (a_dictionary)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,4 +4,4 @@ def simple_delete(a_dictionary, key=""):
     """Delete a key and its corresponding value from the dictionary if present."""
     if a_dictionary.get(key) is not None:
         del a_dictionary[key]
-    return a_dictionary
+    return (a_dictionary)
